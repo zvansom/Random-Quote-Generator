9b71aaff3660cdaab04503dc20efa791aca0fce6
Zach VanSomeren <23041336+zvansom@users.noreply.github.com>
2017-08-13 17:59:43 -0600

    index.html and main.js initial upload
    Html and JS are both fully functional as intended.  The array of quotes is currently hardcoded in the JS file.  Eventually I would like to change this to an API, but for now this will suffice.
    
    No stylesheet created yet.

---
+++ main.js
@@ -0,0 +1,519 @@
+// Script begins at line 500.
+// Hard coded array of quotes.
+var quotes = [{
+"quoteText": "Learn from yesterday, live for today, hope for tomorrow.",
+"quoteAuthor": "Albert Einstein"
+}, {
+"quoteText": "A weed is no more than a flower in disguise.",
+"quoteAuthor": "James Lowell"
+}, {
+"quoteText": "Do, or do not. There is no try.",
+"quoteAuthor": "Yoda"
+}, {
+"quoteText": "All serious daring starts from within.",
+"quoteAuthor": "Harriet Beecher Stowe"
+}, {
+"quoteText": "The best teacher is experience learned from failures.",
+"quoteAuthor": "Byron Pulsifer"
+}, {
+"quoteText": "Think how hard physics would be if particles could think.",
+"quoteAuthor": "Murray Gell-Mann"
+}, {
+"quoteText": "Love is the flower you've got to let grow.",
+"quoteAuthor": "John Lennon"
+}, {
+"quoteText": "Don't wait. The time will never be just right.",
+"quoteAuthor": "Napoleon Hill"
+}, {
+"quoteText": "One fails forward toward success.",
+"quoteAuthor": "Charles Kettering"
+}, {
+"quoteText": "Time is the wisest counsellor of all.",
+"quoteAuthor": "Pericles"
+}, {
+"quoteText": "You give before you get.",
+"quoteAuthor": "Napoleon Hill"
+}, {
+"quoteText": "Wisdom begins in wonder.",
+"quoteAuthor": "Socrates"
+}, {
+"quoteText": "Without courage, wisdom bears no fruit.",
+"quoteAuthor": "Baltasar Gracian"
+}, {
+"quoteText": "Change in all things is sweet.",
+"quoteAuthor": "Aristotle"
+}, {
+"quoteText": "What you fear is that which requires action to overcome.",
+"quoteAuthor": "Byron Pulsifer"
+}, {
+"quoteText": "The best teacher is experience learned from failures.",
+"quoteAuthor": "Byron Pulsifer"
+}, {
+"quoteText": "When performance exceeds ambition, the overlap is called success.",
+"quoteAuthor": "Cullen Hightower"
+}, {
+"quoteText": "When deeds speak, words are nothing.",
+"quoteAuthor": "African proverb"
+}, {
+"quoteText": "Real magic in relationships means an absence of judgement of others.",
+"quoteAuthor": "Wayne Dyer"
+}, {
+"quoteText": "When performance exceeds ambition, the overlap is called success.",
+"quoteAuthor": "Cullen Hightower"
+}, {
+"quoteText": "I never think of the future. It comes soon enough.",
+"quoteAuthor": "Albert Einstein"
+}, {
+"quoteText": "Skill to do comes of doing.",
+"quoteAuthor": "Ralph Emerson"
+}, {
+"quoteText": "Wisdom is the supreme part of happiness.",
+"quoteAuthor": "Sophocles"
+}, {
+"quoteText": "I believe that every person is born with talent.",
+"quoteAuthor": "Maya Angelou"
+}, {
+"quoteText": "Important principles may, and must, be inflexible.",
+"quoteAuthor": "Abraham Lincoln"
+}, {
+"quoteText": "The undertaking of a new action brings new strength.",
+"quoteAuthor": "Richard Evans"
+}, {
+"quoteText": "I believe that every person is born with talent.",
+"quoteAuthor": "Maya Angelou"
+}, {
+"quoteText": "The years teach much which the days never know.",
+"quoteAuthor": "Ralph Emerson"
+}, {
+"quoteText": "Our distrust is very expensive.",
+"quoteAuthor": "Ralph Emerson"
+}, {
+"quoteText": "All know the way; few actually walk it.",
+"quoteAuthor": "Bodhidharma"
+}, {
+"quoteText": "Great talent finds happiness in execution.",
+"quoteAuthor": "Johann Wolfgang von Goethe"
+}, {
+"quoteText": "Faith in oneself is the best and safest course.",
+"quoteAuthor": "Michelangelo"
+}, {
+"quoteText": "Courage is going from failure to failure without losing enthusiasm.",
+"quoteAuthor": "Winston Churchill"
+}, {
+"quoteText": "The two most powerful warriors are patience and time.",
+"quoteAuthor": "Leo Tolstoy"
+}, {
+"quoteText": "Anticipate the difficult by managing the easy.",
+"quoteAuthor": "Lao Tzu"
+}, {
+"quoteText": "Those who are free of resentful thoughts surely find peace.",
+"quoteAuthor": "Buddha"
+}, {
+"quoteText": "Talk doesn't cook rice.",
+"quoteAuthor": "Chinese proverb"
+}, {
+"quoteText": "A short saying often contains much wisdom.",
+"quoteAuthor": "Sophocles"
+}, {
+"quoteText": "The day is already blessed, find peace within it.",
+"quoteAuthor": ""
+}, {
+"quoteText": "It takes both sunshine and rain to make a rainbow.",
+"quoteAuthor": ""
+}, {
+"quoteText": "A beautiful thing is never perfect.",
+"quoteAuthor": ""
+}, {
+"quoteText": "Only do what your heart tells you.",
+"quoteAuthor": "Princess Diana"
+}, {
+"quoteText": "Life is movement-we breathe, we eat, we walk, we move!",
+"quoteAuthor": "John Pierrakos"
+}, {
+"quoteText": "No one can make you feel inferior without your consent.",
+"quoteAuthor": "Eleanor Roosevelt"
+}, {
+"quoteText": "One fails forward toward success.",
+"quoteAuthor": "Charles Kettering"
+}, {
+"quoteText": "Argue for your limitations, and sure enough theyre yours.",
+"quoteAuthor": "Richard Bach"
+}, {
+"quoteText": "Luck is what happens when preparation meets opportunity.",
+"quoteAuthor": "Seneca"
+}, {
+"quoteText": "Victory belongs to the most persevering.",
+"quoteAuthor": "Napoleon Bonaparte"
+}, {
+"quoteText": "Once you choose hope, anythings possible.",
+"quoteAuthor": "Christopher Reeve"
+}, {
+"quoteText": "Love all, trust a few, do wrong to none.",
+"quoteAuthor": "William Shakespeare"
+}, {
+"quoteText": "In order to win, you must expect to win.",
+"quoteAuthor": "Richard Bach"
+}, {
+"quoteText": "A goal is a dream with a deadline.",
+"quoteAuthor": "Napoleon Hill"
+}, {
+"quoteText": "You can do it if you believe you can!",
+"quoteAuthor": "Napoleon Hill"
+}, {
+"quoteText": "Set your goals high, and don't stop till you get there.",
+"quoteAuthor": "Bo Jackson"
+}, {
+"quoteText": "Genius is one percent inspiration and ninety-nine percent perspiration.",
+"quoteAuthor": "Thomas Edison"
+}, {
+"quoteText": "Every new day is another chance to change your life.",
+"quoteAuthor": ""
+}, {
+"quoteText": "Smile, breathe, and go slowly.",
+"quoteAuthor": "Thich Nhat Hanh"
+}, {
+"quoteText": "Nobody will believe in you unless you believe in yourself.",
+"quoteAuthor": "Liberace"
+}, {
+"quoteText": "Be kind whenever possible. It is always possible.",
+"quoteAuthor": "Dalai Lama"
+}, {
+"quoteText": "Do more than dream: work.",
+"quoteAuthor": "William Arthur Ward"
+}, {
+"quoteText": "No man was ever wise by chance.",
+"quoteAuthor": "Seneca"
+}, {
+"quoteText": "Some pursue happiness, others create it.",
+"quoteAuthor": ""
+}, {
+"quoteText": "It's easier to see the mistakes on someone else's paper.",
+"quoteAuthor": ""
+}, {
+"quoteText": "Think how hard physics would be if particles could think.",
+"quoteAuthor": "Murray Gell-Mann"
+}, {
+"quoteText": "Well begun is half done.",
+"quoteAuthor": "Aristotle"
+}, {
+"quoteText": "He that is giddy thinks the world turns round.",
+"quoteAuthor": "William Shakespeare"
+}, {
+"quoteText": "Don't ruin the present with the ruined past.",
+"quoteAuthor": "Ellen Gilchrist"
+}, {
+"quoteText": "Do something wonderful, people may imitate it.",
+"quoteAuthor": "Albert Schweitzer"
+}, {
+"quoteText": "We do what we do because we believe.",
+"quoteAuthor": ""
+}, {
+"quoteText": "Great talent finds happiness in execution.",
+"quoteAuthor": "Johann Wolfgang von Goethe"
+}, {
+"quoteText": "Do one thing every day that scares you.",
+"quoteAuthor": "Eleanor Roosevelt"
+}, {
+"quoteText": "If you cannot be silent be brilliant and thoughtful.",
+"quoteAuthor": "Byron Pulsifer"
+}, {
+"quoteText": "Smile, breathe, and go slowly.",
+"quoteAuthor": "Thich Nhat Hanh"
+}, {
+"quoteText": "Who looks outside, dreams; who looks inside, awakes.",
+"quoteAuthor": "Carl Jung"
+}, {
+"quoteText": "What we think, we become.",
+"quoteAuthor": "Buddha"
+}, {
+"quoteText": "The shortest answer is doing.",
+"quoteAuthor": "Lord Herbert"
+}, {
+"quoteText": "All our knowledge has its origins in our perceptions.",
+"quoteAuthor": "Leonardo da Vinci"
+}, {
+"quoteText": "He is able who thinks he is able.",
+"quoteAuthor": "Buddha"
+}, {
+"quoteText": "The harder you fall, the higher you bounce.",
+"quoteAuthor": ""
+}, {
+"quoteText": "Trusting our intuition often saves us from disaster.",
+"quoteAuthor": "Anne Wilson Schaef"
+}, {
+"quoteText": "Truth is powerful and it prevails.",
+"quoteAuthor": "Sojourner Truth"
+}, {
+"quoteText": "Talk doesn't cook rice.",
+"quoteAuthor": "Chinese proverb"
+}, {
+"quoteText": "Light tomorrow with today!",
+"quoteAuthor": "Elizabeth Browning"
+}, {
+"quoteText": "Silence is a fence around wisdom.",
+"quoteAuthor": "German proverb"
+}, {
+"quoteText": "Society develops wit, but its contemplation alone forms genius.",
+"quoteAuthor": "Madame de Stael"
+}, {
+"quoteText": "Real magic in relationships means an absence of judgement of others.",
+"quoteAuthor": "Wayne Dyer"
+}, {
+"quoteText": "The years teach much which the days never know.",
+"quoteAuthor": "Ralph Emerson"
+}, {
+"quoteText": "We can only learn to love by loving.",
+"quoteAuthor": "Iris Murdoch"
+}, {
+"quoteText": "The simplest things are often the truest.",
+"quoteAuthor": "Richard Bach"
+}, {
+"quoteText": "What you give is what you get.",
+"quoteAuthor": "Byron Pulsifer"
+}, {
+"quoteText": "Everyone smiles in the same language.",
+"quoteAuthor": ""
+}, {
+"quoteText": "A short saying often contains much wisdom.",
+"quoteAuthor": "Sophocles"
+}, {
+"quoteText": "Yesterday I dared to struggle. Today I dare to win.",
+"quoteAuthor": "Bernadette Devlin"
+}, {
+"quoteText": "Victory belongs to the most persevering.",
+"quoteAuthor": "Napoleon Bonaparte"
+}, {
+"quoteText": "No alibi will save you from accepting the responsibility.",
+"quoteAuthor": "Napoleon Hill"
+}, {
+"quoteText": "If you can dream it, you can do it.",
+"quoteAuthor": "Walt Disney"
+}, {
+"quoteText": "From error to error one discovers the entire truth.",
+"quoteAuthor": "Sigmund Freud"
+}, {
+"quoteText": "It is better to travel well than to arrive.",
+"quoteAuthor": "Buddha"
+}, {
+"quoteText": "Life shrinks or expands in proportion to one's courage.",
+"quoteAuthor": "Anais Nin"
+}, {
+"quoteText": "You have to believe in yourself.",
+"quoteAuthor": "Sun Tzu"
+}, {
+"quoteText": "Our intention creates our reality.",
+"quoteAuthor": "Wayne Dyer"
+}, {
+"quoteText": "Silence is a true friend who never betrays.",
+"quoteAuthor": "Confucius"
+}, {
+"quoteText": "Character develops itself in the stream of life.",
+"quoteAuthor": "Johann Wolfgang von Goethe"
+}, {
+"quoteText": "From little acorns mighty oaks do grow.",
+"quoteAuthor": "American proverb"
+}, {
+"quoteText": "You have to believe in yourself.",
+"quoteAuthor": "Sun Tzu"
+}, {
+"quoteText": "You can't stop the waves, but you can learn to surf.",
+"quoteAuthor": "Jon Kabat-Zinn"
+}, {
+"quoteText": "Reality does not conform to the ideal, but confirms it.",
+"quoteAuthor": "Gustave Flaubert"
+}, {
+"quoteText": "Speak low, if you speak love.",
+"quoteAuthor": "William Shakespeare"
+}, {
+"quoteText": "A really great talent finds its happiness in execution.",
+"quoteAuthor": "Johann Wolfgang von Goethe"
+}, {
+"quoteText": "Reality leaves a lot to the imagination.",
+"quoteAuthor": "John Lennon"
+}, {
+"quoteText": "Our intention creates our reality.",
+"quoteAuthor": "Wayne Dyer"
+}, {
+"quoteText": "Society develops wit, but its contemplation alone forms genius.",
+"quoteAuthor": "Madame de Stael"
+}, {
+"quoteText": "The greatest remedy for anger is delay.",
+"quoteAuthor": "Seneca"
+}, {
+"quoteText": "Growth itself contains the germ of happiness.",
+"quoteAuthor": "Pearl Buck"
+}, {
+"quoteText": "You can do what's reasonable or you can decide what's possible.",
+"quoteAuthor": ""
+}, {
+"quoteText": "Nothing strengthens authority so much as silence.",
+"quoteAuthor": "Leonardo da Vinci"
+}, {
+"quoteText": "Wherever you go, go with all your heart.",
+"quoteAuthor": "Confucius"
+}, {
+"quoteText": "The only real valuable thing is intuition.",
+"quoteAuthor": "Albert Einstein"
+}, {
+"quoteText": "I believe that every person is born with talent.",
+"quoteAuthor": "Maya Angelou"
+}, {
+"quoteText": "Good luck is another name for tenacity of purpose.",
+"quoteAuthor": "Ralph Emerson"
+}, {
+"quoteText": "Rainbows apologize for angry skies.",
+"quoteAuthor": "Sylvia Voirol"
+}, {
+"quoteText": "Friendship isn't a big thing. It's a million little things.",
+"quoteAuthor": ""
+}, {
+"quoteText": "Time is the most valuable thing a man can spend.",
+"quoteAuthor": "Theophrastus"
+}, {
+"quoteText": "Whatever happens, take responsibility.",
+"quoteAuthor": "Tony Robbins"
+}, {
+"quoteText": "Experience is simply the name we give our mistakes.",
+"quoteAuthor": "Oscar Wilde"
+}, {
+"quoteText": "I think and that is all that I am.",
+"quoteAuthor": "Wayne Dyer"
+}, {
+"quoteText": "A good plan today is better than a perfect plan tomorrow.",
+"quoteAuthor": ""
+}, {
+"quoteText": "Be as you wish to seem.",
+"quoteAuthor": "Socrates"
+}, {
+"quoteText": "If the shoe doesn't fit, must we change the foot?",
+"quoteAuthor": "Gloria Steinem"
+}, {
+"quoteText": "Each day provides its own gifts.",
+"quoteAuthor": "Marcus Aurelius"
+}, {
+"quoteText": "While we stop to think, we often miss our opportunity.",
+"quoteAuthor": "Publilius Syrus"
+}, {
+"quoteText": "Life isn't about finding yourself. Life is about creating yourself.",
+"quoteAuthor": "Bernard Shaw"
+}, {
+"quoteText": "To bring anything into your life, imagine that it's already there.",
+"quoteAuthor": "Richard Bach"
+}, {
+"quoteText": "Begin to weave and God will give you the thread.",
+"quoteAuthor": "German proverb"
+}, {
+"quoteText": "The more you know yourself, the more you forgive yourself.",
+"quoteAuthor": "Confucius"
+}, {
+"quoteText": "Someone remembers, someone cares; your name is whispered in someone's prayers.",
+"quoteAuthor": ""
+}, {
+"quoteText": "Without faith, nothing is possible. With it, nothing is impossible.",
+"quoteAuthor": "Mary Bethune"
+}, {
+"quoteText": "What worries you masters you.",
+"quoteAuthor": "Haddon Robinson"
+}, {
+"quoteText": "Once we accept our limits, we go beyond them.",
+"quoteAuthor": "Albert Einstein"
+}, {
+"quoteText": "Don't be pushed by your problems; be led by your dreams.",
+"quoteAuthor": ""
+}, {
+"quoteText": "Whatever we expect with confidence becomes our own self-fulfilling prophecy.",
+"quoteAuthor": "Brian Tracy"
+}, {
+"quoteText": "Everything you can imagine is real.",
+"quoteAuthor": "Pablo Picasso"
+}, {
+"quoteText": "The shortest answer is doing.",
+"quoteAuthor": "Lord Herbert"
+}, {
+"quoteText": "A beautiful thing is never perfect.",
+"quoteAuthor": ""
+}, {
+"quoteText": "Fear is a darkroom where negatives develop.",
+"quoteAuthor": "Usman Asif"
+}, {
+"quoteText": "The simplest things are often the truest.",
+"quoteAuthor": "Richard Bach"
+}, {
+"quoteText": "The truest wisdom is a resolute determination.",
+"quoteAuthor": "Napoleon Bonaparte"
+}, {
+"quoteText": "Life is the flower for which love is the honey.",
+"quoteAuthor": "Victor Hugo"
+}, {
+"quoteText": "Freedom is the right to live as we wish.",
+"quoteAuthor": "Epictetus"
+}, {
+"quoteText": "Change your thoughts, change your life!",
+"quoteAuthor": ""
+}, {
+"quoteText": "Never ignore a gut feeling, but never believe that it's enough.",
+"quoteAuthor": "Robert Heller"
+}, {
+"quoteText": "Loss is nothing else but change,and change is Natures delight.",
+"quoteAuthor": "Marcus Aurelius"
+}, {
+"quoteText": "Someone is special only if you tell them.",
+"quoteAuthor": "Byron Pulsifer"
+}, {
+"quoteText": "Great talent finds happiness in execution.",
+"quoteAuthor": "Johann Wolfgang von Goethe"
+}, {
+"quoteText": "Today is the tomorrow you worried about yesterday.",
+"quoteAuthor": ""
+}, {
+"quoteText": "There is no way to happiness, happiness is the way.",
+"quoteAuthor": "Thich Nhat Hanh"
+}, {
+"quoteText": "The day always looks brighter from behind a smile.",
+"quoteAuthor": ""
+}, {
+"quoteText": "A beautiful thing is never perfect.",
+"quoteAuthor": ""
+}, {
+"quoteText": "No alibi will save you from accepting the responsibility.",
+"quoteAuthor": "Napoleon Hill"
+}, {
+"quoteText": "Argue for your limitations, and sure enough theyre yours.",
+"quoteAuthor": "Richard Bach"
+}, {
+"quoteText": "You can observe a lot just by watching.",
+"quoteAuthor": "Yogi Berra"
+}, {
+"quoteText": "Reality leaves a lot to the imagination.",
+"quoteAuthor": "John Lennon"
+}, {
+"quoteText": "A stumble may prevent a fall.",
+"quoteAuthor": ""
+}, {
+"quoteText": "He who talks more is sooner exhausted.",
+"quoteAuthor": "Lao Tzu"
+}, {
+"quoteText": "Well begun is half done.",
+"quoteAuthor": "Aristotle"
+}];
+
+// Start Script.
+$(document).ready(function() {
+  // On button click.
+  $("button").on("click", function() {
+    // Set random value to create an index.
+    var randomIndex = Math.floor(Math.random() * 166);
+    // Send html new quote.
+    $("#message").html(function() {
+      return "<p>" + quotes[randomIndex].quoteText + "</p>";
+    });
+    // Send html Author.
+    $("#author").html(function() {
+      if (returnAuthor === ""){
+        return "<p>- Anonymous</p>";
+      } else {
+        return "<p>- " + quotes[randomIndex].quoteAuthor + "</p>";
+      }
+    });
+  });
+});
